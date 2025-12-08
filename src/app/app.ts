a2a6a89d772a155360cfdd23374315eff0bbc60e
Ednei Filho <ednei.ag.filho@gmail.com>
2025-12-08 20:05:02 -0300

    continuing the basics

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('web-active-ultra');
+  protected readonly title = signal('active-ultra-front-end');
 }
